08ec1580813cff24283c5631b430a6be3bf0bb5d
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2024-10-09 20:28:42 -0300

    fix: bug in rendering map and create app bar to the map

--- src/features/map/pages/MapPage.tsx
+++ src/features/map/pages/MapPage.tsx
@@ -1,8 +1,9 @@
 // MapPage.tsx
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-
 // Configura o ícone do marcador
 const icon = new L.Icon({
   iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
@@ -16,19 +17,33 @@ const MapPage: React.FC = () => {
   const position: [number, number] = [51.505, -0.09]; // Coordenadas do centro do mapa
 
   return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      style={{ height: "100vh", width: "100vw" }}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position} icon={icon}>
-        <Popup>Você está aqui!</Popup>
-      </Marker>
-    </MapContainer>
+    <Box sx={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      {/* AppBar do MUI */}
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Mapa com AppBar
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      {/* Mapa */}
+      <Box sx={{ flexGrow: 1 }}>
+        <MapContainer
+          center={position}
+          zoom={13}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position} icon={icon}>
+            <Popup>Você está aqui!</Popup>
+          </Marker>
+        </MapContainer>
+      </Box>
+    </Box>
   );
 };
 
