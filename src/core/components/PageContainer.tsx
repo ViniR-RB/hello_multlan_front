f28173d852b7ba20255fa0023aab19fb9a10923b
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2024-11-18 20:39:26 -0300

    style: create page container from style global

--- src/core/components/PageContainer.tsx
+++ src/core/components/PageContainer.tsx
@@ -1,18 +1,24 @@
-import { Box } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 import React from "react";
 
-const PageContainer: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+interface PageContainerProps {
+  children: React.ReactNode;
+  title: string;
+}
+
+const PageContainer: React.FC<PageContainerProps> = ({ children, title }) => {
   return (
     <Box
       sx={{
-        maxWidth: "1024px", // Limita a largura máxima
-        width: "100%", // Garante que a largura ocupe 100% do espaço disponível
-        mx: "auto", // Centraliza horizontalmente
-        p: 3, // Adiciona um padding ao redor do conteúdo
+        width: "100%",
+        mx: "auto",
+        p: 3,
       }}
     >
+      <Typography variant="h6" color="textPrimary" sx={{ mb: 1 }}>
+        {title}
+      </Typography>
+      <Divider flexItem={true} orientation="horizontal" sx={{ mb: 2 }} />
       {children}
     </Box>
   );
