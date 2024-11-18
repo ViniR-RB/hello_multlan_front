4b92998ca450680548e306d96d970e386b405ce1
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2024-11-18 20:38:15 -0300

    feat: creates application layouts

---
+++ src/core/layouts/CoreLayout.tsx
@@ -0,0 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Outlet } from "react-router-dom";
+import { AuthProvider } from "../context/AuthContext";
+import { SnackbarProvider } from "../context/SnackBarContext";
+const queryClient = new QueryClient();
+
+export default function CoreLayout() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <SnackbarProvider>
+          <Outlet />
+        </SnackbarProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+}
---
+++ src/core/layouts/MainLayout.tsx
@@ -0,0 +1,41 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import { AppBar, Box, IconButton, Toolbar } from "@mui/material";
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import CustomDrawer from "../components/CustomDrawer";
+const MainLayout: React.FC = () => {
+  const drawerWidth = 240;
+  const [open, setOpen] = useState<boolean>(false);
+  const handleDrawerToggle = () => {
+    setOpen(!open);
+  };
+  return (
+    <Box sx={{ display: "flex" }}>
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box component="span" sx={{ flexGrow: 1 }}>
+            Hello Multlan
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <CustomDrawer open={open} drawerWidth={drawerWidth} />
+      <Box component="main" sx={{ flexGrow: 1, pt: 10 }}>
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default MainLayout;
